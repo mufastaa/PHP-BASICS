6271a767f2cb1eb332f2b2e1777afb7fa326322f
mufastaa <ishimwep21@gmail.com>
2021-11-17 06:24:33 -0800

    let's print simple student details

---
+++ datatype.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html>
+<head>
+    <title>
+        Data Types
+    </title>
+</head>
+<body>
+
+<h1>Exercise 1: Print Student Details</h1>
+
+<?php
+
+    $studentName = "mufasa";
+    $classSection = "L3";
+    $age = 25;
+    $sex = "Male";
+    $contact = "0788888";
+
+    $maths = 0;
+    $science = 0;
+    $total = 0;
+    $totalMarks = 200;
+    $percentage =  0;
+    $passed = null;
+
+?>
+
+
+<h2>Student Information:</h2>
+
+<table border="2">
+    <tr>
+        <td>Name</td>
+        <td>Class</td>
+        <td>Age</td>
+        <td>Sex</td>
+        <td>Contact</td>
+    </tr>
+    <tr>
+        <td><?= $studentName ?></td>
+        <td><?= $classSection ?></td>
+        <td><?= $age ?></td>
+        <td><?= $sex ?></td>
+        <td><?= $contact ?></td>
+    </tr>
+</table>
+
+<h2>Exams Attended:</h2>
+<?php
+    $maths = 56;
+    $science = 78;
+    $total = $maths + $science;
+    $examAttended = ($total > 0) ? true : false;
+?>
+
+Exam Attended : <?= ($examAttended) ? "Yes Attended" : "Not Attended"; ?> <br>
+
+<h2>Final Marks:</h2>
+Total Marks : <?= $total ?>
+
+<h2>Percentage:</h2>
+
+<?php
+    $percentage = ( $total / $totalMarks) * 100;
+?>
+Percentage : <?= $percentage ?>
+
+<h2>Result:</h2>
+Final Result <?= ($percentage >= 60)  ? "Passed" : "Failed"; ?>
+
+</body>
+</html>
